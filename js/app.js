93c2095b1e12d7cfcca0bb6e5b6602e022572ee1
Federico <federicoaacevedo@hotmail.com>
2021-12-05 11:42:42 -0300

    Paginador

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 const resultado = document.querySelector("#resultado");
 const formulario = document.querySelector("#formulario");
 
+const registrosPorPagina = 40; 
+
+let totalPaginas;
+
 formulario.addEventListener("submit", validarFormulario);
 
 function validarFormulario(e) {
@@ -49,18 +53,54 @@ function mostrarAlerta(mensaje) {
   }
 }
 
-function buscarImagenes(termino){
-
+function buscarImagenes() {
+ 
+    const termino = document.querySelector('#termino').value;
+ 
+ 
     const key = '24646168-2a770a2ec9aa619292b395e35';
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}`;
-
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
+ 
     fetch(url)
-    .then(respuesta => respuesta.json())
-    .then(resultado => {
-        mostrarImagenes(resultado.hits);
-    })
+        .then(respuesta => respuesta.json())
+        .then(resultado => {
+           
+             totalPaginas = calcularPaginas(resultado.totalHits);
+             console.log(totalPaginas);
+             mostrarImagenes(resultado.hits);
+        })
+}
+
+function calcularPaginas(total){
+    return parseInt(Math.ceil(total / registrosPorPagina));
 }
 
 function mostrarImagenes(imagenes){
     console.log(imagenes);
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    //Iterar sobre el arreglo de imagenes
+    imagenes.forEach(imagen => {
+        const { previewURL, likes, views, largeImageURL } = imagen; 
+
+        resultado.innerHTML +=`
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class= "w-full" src="${previewURL}">
+
+                    <div class="p-4">
+                        <p class="font-bold">${likes} <span class="font-light">Me gusta </span></p>
+                        <p class="font-bold">${views} <span class="font-light">Veces vistas</span></p>
+
+                        <a
+                        class="block w-full bg-blue-800 hover:bg-blue-500 text-white upperCase font-bold text-center rounded mt-5 p-1"
+                        href="${largeImageURL}" target="_blank">Ver imagen</a>
+
+                    </div>
+                </div>
+            <div/>    
+            `
+    });
 }
